2cc40a5d551f6e1885b2e15a88d1485f59ba50bf
Gurnehmat Kaur Dhindsa <gurnehmat.dhindsa@gmail.com>
2020-11-24 01:16:28 +0530

    fetch data

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,60 @@
-$(document).ready(handle)
+let URL = `https://automation-script-worker.herokuapp.com/scripts/?limit=10&offset=0`;
 
-/*Accordion*/
-function handle() {
+function addDataToDOM(data) {
+    for (let i = 0; i < data.results.length; i++) {
+        let current = data.results[i];
+
+        const card = ` <div class="card" loading="lazy">
+    <div class="card-main">
+        <img src="${current.creator_dp}" alt="" class="circular">
+        <div class="card-main-text">
+            <h2>${current.name}</h2>
+            <h3> ${current.made_by}</h3>
+            <span class="card-m-b">
+                <p>${current.added}</p>
+                <span>
+                    <a href="${current.url}" class="card-btn">Download</a>
+                    <a href="${current.url}" class="card-btn">View</a>
+                </span>
+            </span>
+        </div>
+    </div>
+    <div class="card-collapsible hide">
+        <div class="hr"></div>
+        <div>
+            <table>
+                <tr>
+                    <th>
+                    Category: 
+                    </th>
+                    <td class="cell">
+                        ${current.category}
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        Description: 
+                    </th>
+                    <td>
+                        ${current.description}
+                    </td>
+                </tr>
+            </table>
+        </div>    
+    </div>
+    <div class="chevron-c">
+        <i class="fas fa-chevron-down"></i>
+    </div>
+    
+    <div class="chevron-c hide">
+        <i class="fas fa-chevron-up"></i>
+    </div>
+</div>`
+
+        $('.container').append(card);
+    }
+    
+
     $(`.chevron-c>.fa-chevron-down`).click(function () {
         $(this).parent().siblings('.card-collapsible').removeClass('hide');
         $(this).parent().addClass('hide');
@@ -12,19 +65,49 @@ function handle() {
         $(this).parent().addClass('hide');
         $(this).parent().siblings('.chevron-c').removeClass('hide')
     })
-    $('.scroll-top').click(function(){
-        $(window).scrollTop(0); 
-    })
+
 }
+
+
+/*Function to get data*/
+function getPost() {
+    jQuery.get(URL, function (data, status) {
+        addDataToDOM(data);
+        URL = data.next;
+    });
+}
+
+    $('.scroll-top').click(function () {
+        $(window).scrollTop(0);
+    })
+    /*
+    $(function() {
+        $('.jscroll').jscroll();    
+    });
+    */
+   
+/*run the function the first time*/
+getPost();
 /**/
+
+
 $(window).scroll(function (event) {
-    var scroll = $(window).scrollTop();
-  
-    if(scroll > 100){
+    var scrollTop = $(document).scrollTop();
+
+    /*Hiding and Showing scroll to top button*/
+    if (scrollTop > 100) {
         $('.scroll-top').removeClass('hide')
-     
+
+    } else {
+        $('.scroll-top').addClass('hide')
     }
-    else{
-        $('.scroll-top').addClass('hide') 
+    
+    /*If almost scrolled to bottom, fetch data again*/
+    let winHeight=$(window).height()
+    let docHeight= $(document).height()
+  
+    if(scrollTop+winHeight>=docHeight-100) {
+        getPost()
     }
+
 });
