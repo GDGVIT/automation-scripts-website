8896e3f05f802beaa3dd41e44a25beec8e3e7f09
Gurnehmat Kaur Dhindsa <gurnehmat.dhindsa@gmail.com>
2020-11-25 12:07:46 +0530

    check for verified functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,6 +9,9 @@ $(document).ready(function () {
 let URL = `https://automation-script-worker.herokuapp.com/scripts/?limit=10&offset=0`;
 /*Global variable to mark search on or off and trigger general display accordingly*/
 let search = false;
+let modalOpen=false;
+let verified =`<i class="fas fa-check-circle"></i>`
+let notVerified='', verifiedHtml;
 
 
 /*Extract date from timestamp */
@@ -28,80 +31,100 @@ function addDataToDOM(data) {
         let current = data.results[i];
         /*Call extract date function to get date from timestamp*/
         let date = extractDate(current.added);
-        const card = ` 
-        <div class="card" loading="lazy">
+        if(current.verified)
+        verifiedHtml=verified;
+        else
+        verifiedHtml=notVerified;
+        const card = `<div class="card" loading="lazy">
         <div class="card-main">
-        <img src="${current.creator_dp}" alt="" class="circular">
-        <div class="card-main-text">
-        <div class="script-name">
-        <h2>${current.name}</h2>
-        <i class="fas fa-check-circle"></i>
-        </div>
-            
-            <h3> ${current.made_by}</h3>
-            <span class="card-m-b">
-                <p>${date}</p>
-                <span class="btns">
-                    <a href="${current.url}" class="card-btn" target="_blank" >Download</a>
-                    <a href="${current.url}" class="card-btn" target="_blank">View</a>
-                </span>
-            </span>
-            </div>
-            </div>
-            <div class="card-collapsible hide">
-                <div class="hr"></div>
-                <div>
-                <table>
-                <tr>
-                    <th>
-                    Category: 
-                    </th>
-                    <td class="cell">
-                        ${current.category}
-                    </td>
-                </tr>
-                <tr>
-                    <th>
-                        Description: 
-                    </th>
-                    <td>
-                        ${current.description}
-                    </td>
-                </tr>
-            </table>
-        </div>    
-    </div>
-    <div class="chevron-c">
-    <i class="fas fa-chevron-down"></i>
-    </div>
+            <img src="${current.creator_dp}" alt="" class="circular">
+            <div class="card-main-text">
+                <div class="script-name">
+                    <h2>${current.name}</h2>
+                    ${verifiedHtml}
+                    
+                </div>
     
-  
-</div>`
+                <h3> ${current.made_by}</h3>
+                <span class="card-m-b">
+                    <p>${date}</p>
+                    <span class="btns">
+                        <a href="${current.url}" class="card-btn" target="_blank">Download</a>
+                        <a href="${current.url}" class="card-btn" target="_blank">View</a>
+                    </span>
+                </span>
+            </div>
+        </div>
+        <div class="card-collapsible hide">
+            <div class="hr"></div>
+            <div>
+                <table>
+                    <tr>
+                        <th>
+                            Category:
+                        </th>
+                        <td class="cell">
+                            ${current.category}
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            Description:
+                        </th>
+                        <td>
+                            ${current.description}
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            
+        </div>
+        <div class="chevron-c">
+            <i class="fas fa-chevron-down"></i>
+        </div>
+        <div class="chevron-c hide chevron-c-up">
+                <i class="fas fa-chevron-up"></i>
+        </div>
+    </div> 
+        `
         /*Append card to container */
         $('.container').append(card);
     }
 
     /*Check if chevron is clicked and collapse/ expand card */
-    $(`.chevron-c>.fa-chevron-down`).click(function () {
-        console.log('clicked')
-        $('.modal').removeClass('hide')
-        $('.modal-container').html($(this).closest("div.card"));
+    $(`.fa-chevron-down`).click(function () {
+        let store=$(this).closest('.card').html();
+        store=`<div class="card">${store}</div>`
+        $('#modal').removeClass('hide')
+        $('.modal-container').empty()
+      
+        $('.modal-container').append(store);
         $('.modal-container > .card > .card-collapsible').removeClass('hide')
-
+        $('.modal-container > .card > .chevron-c').addClass('hide')
+        $('.modal-container > .card > .chevron-c-up').removeClass('hide')
     })
+    
+    $(`.fa-chevron-up`).click(function () {
+       $('.close').click()
+    })
+   
 
 }
 
 /*close modal on click of close button*/
 $('.close').click(function () {
-    $('.modal').addClass('hide');
+    $('#modal').addClass('hide');
 })
 
+
 /*Close modal on clicking outside */
+
+  
 
 /*Function to get all scripts data*/
 function getPost() {
     jQuery.get(URL, function (data, status) {
+        console.log(data)
         addDataToDOM(data);
         URL = data.next;
     });
